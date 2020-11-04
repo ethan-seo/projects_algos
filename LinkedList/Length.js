f1485877c8194ec603596c45683efed6494b61f7
Ethan Seo <sung.min.seo.428@gmail.com>
2020-11-03 23:47:03 -0800

    Adding Length.js

---
+++ LinkedList/Length.js
@@ -0,0 +1,45 @@
+// for(var i=0; i<5; i++){
+//     console.log(i)
+// }
+
+// var i = 0;
+// while(i < 5){
+//     console.log(i);
+//     i++;
+// }
+
+// var runner = this.head;
+// while(runner != null){
+
+//     runner = runner.next;
+// }
+
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    length() {
+        var runner = this.head;
+        var count = 0;
+        while (runner != null) {
+            count++;
+            runner = runner.next;
+        }
+        return count;
+    }
+}
+
+var sll1 = new SLL();
+var node1 = new Node(3);
+sll1.head = node1;
+var node2 = new Node(7);
+node1.next = node2;
+console.log(sll1.length());
